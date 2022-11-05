4df575455574b2f39698f39c3d7666935e3850f5
JoseaSosa24 <josea_1998@hotmail.com>
2022-11-05 13:14:15 -0500

    SNT14-Se guardo informacion en la DB

--- config/web/views.py
+++ config/web/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+
 from web.formularios.platos import FormularioPlatos
 from web.formularios.empleados import FormularioEmpleados
+
+from web.models import Platos, Empleados
 
 # Create your views here.
 
@@ -16,6 +19,27 @@ def Plato(request):
     data = {
         'formulario': formulario
     }
+
+    if request.method=="POST":
+        datosFormulario=FormularioPlatos(request.POST)
+        if datosFormulario.is_valid():
+            datosLimpios = datosFormulario.cleaned_data
+            print(datosLimpios)
+            platoNuevo = Platos(
+                nombre = datosLimpios["nombre"],
+                descripcion = datosLimpios["descripcion"],
+                fotografia = datosLimpios["fotografia"],
+                precio = datosLimpios["precio"],
+                tipo = datosLimpios["tipo"]
+            )
+
+            try:
+                platoNuevo.save()
+                print('Platos guardados')
+            except Exception as error:
+                print('Upss', error)
+
+
     return render(request, 'menuPlatos.html', data)
 
 def Empleado(request):
@@ -23,4 +47,25 @@ def Empleado(request):
     data = {
         'formulario': formulario
     }
+
+    if request.method=="POST":
+        datosFormularioEmpleado=FormularioEmpleados(request.POST)
+        if datosFormularioEmpleado.is_valid():
+            datosLimpios = datosFormularioEmpleado.cleaned_data
+            print(datosLimpios)
+            # empleadoNuevo = Empleados(
+            #     nombre = datosLimpios["nombre"],
+            #     apellidos = datosLimpios["apellidos"],
+            #     fotografia = datosLimpios["fotografia"],
+            #     cargo = datosLimpios["cargo"],
+            #     salario = datosLimpios["salario"],
+            #     contacto = datosLimpios["contacto"]
+            # )
+
+            # try:
+            #     empleadoNuevo.save()
+            #     print('Empleado guardados')
+            # except Exception as error:
+            #     print('Upss', error)
+
     return render(request, 'registrarEmpleados.html', data)
